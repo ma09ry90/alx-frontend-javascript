e8031e27333ac920c1006b0fcd60d8ae0d454064
ma09ry90 <mengistumary60@gmail.com>
2024-10-03 09:31:41 +0000

    1es6

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from '../alx-frontend-javascript/utils.js'
+import { uploadPhoto, createUser } from './utils.js'
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
